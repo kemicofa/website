c68b155d68384a2e030e31eed73031237e5058c4
kemicofa <kevin.faulhaber@tuta.io>
2018-12-12 11:47:52 +0100

    fixed more email bugs

--- public/javascripts/materialize.custom.js
+++ public/javascripts/materialize.custom.js
@@ -4,4 +4,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const modalElems = document.querySelectorAll('.modal');
     const modalInstances = M.Modal.init(modalElems, {});
+
+    const form = document.getElementById("contact-modal");
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+        const data = new FormData(form);
+
+        fetch('/email', {
+            headers: {"Content-Type":"application/json"},
+            method: "POST",
+            body: JSON.stringify({
+                firstname: data.get('firstname').trim(),
+                lastname: data.get('lastname').trim(),
+                email: data.get('email').trim(),
+                message: data.get('message').trim()
+            })
+        }).then(res=>{
+            modalInstances[0].close();
+        }).catch(err=>console.warn(err.message))
+
+    })
+
 });
--- routes/index.js
+++ routes/index.js
@@ -24,26 +24,54 @@ router.post('/email', function(req, res, next){
     }
   });
 
-  const {emailfrom, firstname, lastname, message} = req.body;
+  const {email, firstname, lastname, message} = req.body;
 
-  var mailOptions = {
-    from: 'notif@kemicofa.io',
+  const mailOptions = {
+    from: 'kevin@kemicofa.io',
     to: target,
-    subject: `Message from ${firstname} ${lastname} (${emailfrom})`,
+    subject: `Message from ${firstname} ${lastname} (${email})`,
     text: message
   };
+
+  const copyMailOptions = {
+    from: 'kevin@kemicofa.io',
+    to: email,
+    subject: `Email copy from kemicofa.io`,
+    html: `
+      <p>Hey !</p>
+      <p>
+          This an automatic email letting you know that your message
+          was successfully delivered.
+      </p>
+      <p>I'll respond to your email as soon as possible.</p>
+      <br/>
+      <p>Cheers,</p>
+      <p>Kevin</p>
+      <a href="http://kemicofa.io">kemicofa.io</a>
+    `
+  }
 
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
+      res.status(400).json({message: "Unable to send message."})
     } else {
       console.log('Email sent: ' + info.response);
+
+      transporter.sendMail(copyMailOptions, function(error, info){
+
+        if(error){
+          //nothing else to do... not a big deal if this fails...
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+
+        res.status(200).json({message: "Message sent..."});
+
+      })
     }
   });
-
-  res.status(200).json({message: "Message sent..."});
-  //TODO: Send message to recipient
-
 })
 
 module.exports = router;
